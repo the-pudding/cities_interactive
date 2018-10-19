2d51959e3d1c783fbd01a6a1d3b9cdcf5c2df7d0
Matt Daniels <matthew.f.daniels@gmail.com>
2018-10-19 11:06:15 -0400

    here

--- src/js/graphic.js
+++ src/js/graphic.js
@@ -13,58 +13,82 @@ function resize() {}
 function init() {
 
 	//
-
-	var layerMadeVisible = "2015-gte-20-limited";
-
+	var layer_2015 = "full-layer copy"
+	var layerMadeVisible = layer_2015;
+	var deltaMapBuilt = false;
 	var makeMap = true;
 	var map;
 	var compareMap;
 	var beforeContainer  = d3.select(".before-map-container");
-	var deltaModeOn = false;
-	var compareModeOn = false;
-	var presentModeOn = true;
+	var currentMode = "present"
+	var deltaMap;
+	var deltaMapContainer = d3.select(".delta-map-container");
 
 	function changeView(view){
+
+		if(view != "delta-button" && currentMode == "delta" && deltaMapBuilt){
+			map.jumpTo({
+				center: deltaMap.getCenter(),
+				zoom: deltaMap.getZoom(),
+				pitch: deltaMap.getPitch(), // pitch in degrees
+				bearing: deltaMap.getBearing() // bearing in degrees
+			});
+		}
 
 		if(view == "compare-button"){
 			map.resize();
 			beforeContainer.classed("extended",true);
 			beforeContainer.transition().duration(200).style("width","50%").on("end",function(d){
-				compareModeOn = true;
+				currentMode = "current";
 				makeCompareMap();
 				map.resize();
 			});
 		}
-		else if(compareModeOn) {
+		else if(currentMode == "compare") {
 			beforeContainer.classed("extended",false);
 			beforeContainer.transition().duration(200).style("width","0%").on("end",function(d){
-				compareModeOn = false;
 				// compareMap.remove();
 				map.resize();
 			});
 		}
-
 		if(view == "delta-button"){
-			deltaModeOn = true;
-			map.setLayoutProperty("delta-1990-2015-limited", 'visibility', 'visible');
-			map.setLayoutProperty("neg-test-5", 'visibility', 'visible');
+			currentMode = "delta";
+			if(!deltaMapBuilt){
+				delta();
+			}
+			else{
+				deltaMap.jumpTo({
+					center: map.getCenter(),
+					zoom: map.getZoom(),
+					pitch: map.getPitch(), // pitch in degrees
+					bearing: map.getBearing() // bearing in degrees
+				});
+			}
+			deltaMapContainer
+				.transition()
+				.duration(500)
+				.style("transform","translate(0px,0px)");
 		}
 		else{
-			deltaModeOn = false;
+			deltaMapContainer
+				.transition()
+				.duration(500)
+				.style("transform","translate(100%,0px)").on("end",function(d){
+					// deltaMap.remove();
+				});
 			if(view != "compare-button"){
-				map.setLayoutProperty("delta-1990-2015-limited", 'visibility', 'none');
-				map.setLayoutProperty("neg-test-5", 'visibility', 'none');
+				// map.setLayoutProperty("delta-1990-2015-limited", 'visibility', 'none');
+				// map.setLayoutProperty("neg-test-5", 'visibility', 'none');
 			}
 		}
 
 		if(view == "present-button"){
-			presentModeOn = true;
-			map.setLayoutProperty("2015-gte-20-limited", 'visibility', 'visible');
+			currentMode = "present";
+			map.setLayoutProperty(layer_2015, 'visibility', 'visible');
 		}
 		else{
-			presentModeOn = false;
 			if(view != "compare-button"){
-				map.setLayoutProperty("2015-gte-20-limited", 'visibility', 'none');
+				map.setLayoutProperty(layer_2015, 'visibility', 'none');
 			}
 		}
 
@@ -75,10 +99,16 @@ function init() {
 
 		var topTogglesContainer = d3.select(".top-toggles");
 
-		topTogglesContainer.selectAll("p").on("click",function(d){
-			changeView(d3.select(this).attr("class"));
+		topTogglesContainer.selectAll("div").on("click",function(d){
+			changeView(d3.select(this).attr("id"));
+			topTogglesContainer.selectAll("div").select("p").classed("top-toggle-active",false);
+			d3.select(this).select("p").classed("top-toggle-active",true);
+
+
+
 		})
 	}
+
 	function makeCompareMap(){
 
 		compareMap = new mapboxgl.Map({
@@ -104,6 +134,17 @@ function init() {
 
 	}
 
+	function delta(){
+		deltaMapBuilt = true;
+		deltaMap = new mapboxgl.Map({
+			container: 'delta-map',
+			style: 'mapbox://styles/dock4242/cjnfclc780ham2rufqmsufejy?optimize=true',
+			center: map.getCenter(),
+			zoom: map.getZoom(),
+			pitch: map.getPitch(), // pitch in degrees
+			bearing: map.getBearing(), // bearing in degrees
+		});
+	}
 
 	if(makeMap){
 		mapboxgl.accessToken = 'pk.eyJ1IjoiZG9jazQyNDIiLCJhIjoiY2pjazE5eTM2NDl2aDJ3cDUyeDlsb292NiJ9.Jr__XbmAolbLyzPDj7-8kQ';
@@ -161,7 +202,6 @@ function init() {
 			})
 
 		}
-
 
 		map = new mapboxgl.Map({
 			container: 'main-map',
